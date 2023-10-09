edc42d022afa2d1c51d4ff467f8f2fcaae15adb0
enyllaoliveira <enyllaoliveira@gmail.com>
2023-10-09 16:18:03 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+    return(
+        <div className="flex flex-col w-full py-4 items-center justify-center">
+            <h1 className="md:text-4xl text-3x1 font-bold text-white mt-20"> Perfil</h1>
+            <span className="text-white"> Veja meus links 👇</span>
+        </div>
+    )
+}
---
+++ src/pages/networks/index.tsx
@@ -0,0 +1,7 @@
+export function Networks() {
+    return(
+        <div>
+            <h1> Página de redes sociais</h1>
+        </div>
+    )
+}
